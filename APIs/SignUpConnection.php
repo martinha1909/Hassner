15f4fd9cc6234ae54dbfd508dd486c3048e79216
martinha1909 <58370282+martinha1909@users.noreply.github.com>
2021-06-30 21:41:18 -0600

    restructured database

--- APIs/SignUpConnection.php
+++ APIs/SignUpConnection.php
@@ -14,16 +14,16 @@
             $email = "";
         $_SESSION['notify'] = signup($conn,$username,$password,$account_type, $email);
         // echo $_SESSION['notify'];
-        if($_SESSION['notify'] == 1)
-            header("Location: ../frontend/login.php");
-        else
-            header("Location: ../frontend/signup.php");
+        // if($_SESSION['notify'] == 1)
+        //     header("Location: ../frontend/login.php");
+        // else
+        //     header("Location: ../frontend/signup.php");
     }
     else
     {
         $_SESSION['notify'] = 2;
         echo $_SESSION['notify'];
-        header("Location: ../frontend/signup.php");
+        // header("Location: ../frontend/signup.php");
     }
 
     closeCon($conn); 
