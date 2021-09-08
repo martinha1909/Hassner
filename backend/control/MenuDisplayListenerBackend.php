fc224444b90c0f058772af9ca112a59d91f0af9a
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2021-09-07 22:11:19 -0600

    removed unused comments

--- backend/control/MenuDisplayListenerBackend.php
+++ backend/control/MenuDisplayListenerBackend.php
@@ -5,25 +5,5 @@
     {
         $_SESSION['display'] = strtoupper($type);
     }
-    // if($type == "Artists")
-    // {
-    //     $_SESSION['display'] = "ARTISTS";
-    // }
-    // else if($type == "Portfolio")
-    // {
-    //     $_SESSION['display'] = "PORTFOLIO";
-    // }
-    // else if($type == "Siliqas")
-    // {
-    //     $_SESSION['display'] = "SILIQAS";
-    // }
-    // else if($type == "Account")
-    // {
-    //     $_SESSION['display'] = "ACCOUNT";
-    // }
-    // else if($type == "Campaign")
-    // {
-    //     $_SESSION['display'] = "CAMPAIGN";
-    // }
     header("Location: ../../frontend/listener/Listener.php");
 ?>
