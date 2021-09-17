e14b754facbe252001697b65d00a4a5f44653ba9
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2021-09-17 13:52:50 -0600

    address comments

--- backend/control/MenuDisplayListenerBackend.php
+++ backend/control/MenuDisplayListenerBackend.php
@@ -1,25 +1,9 @@
 <?php
     session_start();
     $type = $_POST['display_type'];
-    if($type == "Artists")
+    if(in_array($type, ["Artists", "Portfolio", "Siliqas", "Account", "Campaign"]))
     {
-        $_SESSION['display'] = "ARTISTS";
-    }
-    else if($type == "Portfolio")
-    {
-        $_SESSION['display'] = "PORTFOLIO";
-    }
-    else if($type == "Siliqas")
-    {
-        $_SESSION['display'] = "SILIQAS";
-    }
-    else if($type == "Account")
-    {
-        $_SESSION['display'] = "ACCOUNT";
-    }
-    else if($type == "Campaign")
-    {
-        $_SESSION['display'] = "CAMPAIGN";
+        $_SESSION['display'] = strtoupper($type);
     }
     header("Location: ../../frontend/listener/Listener.php");
 ?>
