1df937659fc28d3fb1a496336471de7bcc338fde
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2021-10-03 13:38:23 -0600

    Updated database

--- database/hassner.sql
+++ database/hassner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2021 at 01:07 AM
+-- Generation Time: Oct 03, 2021 at 09:38 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -60,7 +60,7 @@ CREATE TABLE `account` (
 
 INSERT INTO `account` (`username`, `password`, `account_type`, `id`, `Shares`, `balance`, `rate`, `Share_Distributed`, `email`, `billing_address`, `Full_name`, `City`, `State`, `ZIP`, `Card_number`, `Transit_no`, `Inst_no`, `Account_no`, `Swift`, `price_per_share`, `Monthly_shareholder`, `Income`, `Market_cap`, `shares_repurchase`) VALUES
 ('21 Savage', 'artist', 'artist', 6, 0, 0, 0, 0, '21savage@gmail.com', '', '', '', '', '', '', '', '', '', '', 0, 0, 0, 0, 0),
-('88Glam', 'artist', 'artist', 2, 0, 0, 0, 0, '12@gmail.com', '1234', '88 Camino', 'Toronto', 'Ontario', '123456', '1111-2222-3333-4444', '12345', '123', '12345678', 'AAAABBCC', 0, 0, 0, 0, 0),
+('88Glam', 'artist', 'artist', 2, 0, 0, 0, 0, '12@gmail.com', '1234', '88 Camino', 'Toronto', 'Ontario', '123456', '1111-2222-3333-4444', '12345', '123', '12345678', 'AAAABBCC', 0, 0, 0, 0, 3),
 ('kai', 'user', 'user', 4, 0, 0, 0, 0, '123@gmail.com', '', '', '', '', '', '', '', '', '', '', 0, 0, 0, 0, 0),
 ('martin', 'user', 'user', 1, 0, 0, 0, 0, 'martinvuha1909@gmail.com', '2240', 'Vu Ha (Martin)', 'Calgary', 'AB', 'T2N', '1111-2222-3333-4444', '12345', '123', '12345678', 'AAAABBCC', 0, 0, 0, 0, 0),
 ('NAV', 'artist', 'artist', 3, 0, 0, 0, 0, '4321@gmail.com', '', '', '', '', '', '', '', '', '', '', 0, 0, 0, 0, 0),
@@ -96,6 +96,24 @@ CREATE TABLE `buy_order` (
   `siliqas_requested` float NOT NULL,
   `date_posted` varchar(20) NOT NULL,
   `time_posted` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaign`
+--
+
+CREATE TABLE `campaign` (
+  `id` int(11) NOT NULL,
+  `artist_username` varchar(50) NOT NULL,
+  `offering` varchar(20) NOT NULL,
+  `date_posted` varchar(20) NOT NULL,
+  `time_posted` varchar(10) NOT NULL,
+  `date_expires` varchar(10) NOT NULL,
+  `time_expires` varchar(10) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `minimum_ethos` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -157,6 +175,13 @@ ALTER TABLE `buy_order`
   ADD KEY `buy_order_artist_key` (`artist_username`);
 
 --
+-- Indexes for table `campaign`
+--
+ALTER TABLE `campaign`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artist_campaign_key` (`artist_username`);
+
+--
 -- Indexes for table `inject_history`
 --
 ALTER TABLE `inject_history`
@@ -189,6 +214,12 @@ ALTER TABLE `buy_history`
 ALTER TABLE `buy_order`
   ADD CONSTRAINT `buy_order_artist_key` FOREIGN KEY (`artist_username`) REFERENCES `account` (`username`),
   ADD CONSTRAINT `buy_order_user_key` FOREIGN KEY (`user_username`) REFERENCES `account` (`username`);
+
+--
+-- Constraints for table `campaign`
+--
+ALTER TABLE `campaign`
+  ADD CONSTRAINT `artist_campaign_key` FOREIGN KEY (`artist_username`) REFERENCES `account` (`username`);
 
 --
 -- Constraints for table `inject_history`
