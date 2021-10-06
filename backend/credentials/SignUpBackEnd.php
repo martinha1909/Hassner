9f5dedb48f48eed3bfa97252859a8a90363c614f
martinha1909 <58370282+martinha1909@users.noreply.github.com>
2021-10-06 16:11:12 -0600

    Usernames can only have numbers and letters (#51)

--- backend/credentials/SignUpBackEnd.php
+++ backend/credentials/SignUpBackEnd.php
@@ -13,35 +13,46 @@
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
     {
         $_SESSION['status'] = StatusCodes::ErrEmailFormat;
+        $_SESSION['dependencies'] = "FRONTEND";
+        header("Location: ../../frontend/credentials/signup.php");
     }
     else if(!empty($username) && !empty($password) && !empty($email)){
-        $usr_res = searchAccount($conn, $username);
-        $email_res = searchEmail($conn, $email);
-        if($usr_res->num_rows > 0)
+        if(!ctype_alnum($username))
         {
-            $_SESSION['status'] = StatusCodes::ErrUsername;
-            $_SESSION['dependencies'] = "FRONTEND";
-            header("Location: ../../frontend/credentials/signup.php");
-        }
-        else if($email_res->num_rows > 0)
-        {
-            $_SESSION['status'] = StatusCodes::ErrEmailDuplicate;
+            $_SESSION['status'] = StatusCodes::ErrUsernameFormat;
             $_SESSION['dependencies'] = "FRONTEND";
             header("Location: ../../frontend/credentials/signup.php");
         }
         else
         {
-            $_SESSION['status'] = signup($conn, $username, $password, $account_type, $email);
-            if($_SESSION['status'] == StatusCodes::Success)
+            $usr_res = searchAccount($conn, $username);
+            $email_res = searchEmail($conn, $email);
+            if($usr_res->num_rows > 0)
             {
+                $_SESSION['status'] = StatusCodes::ErrUsername;
                 $_SESSION['dependencies'] = "FRONTEND";
-                header("Location: ../../frontend/credentials/login.php");
+                header("Location: ../../frontend/credentials/signup.php");
+            }
+            else if($email_res->num_rows > 0)
+            {
+                $_SESSION['status'] = StatusCodes::ErrEmailDuplicate;
+                $_SESSION['dependencies'] = "FRONTEND";
+                header("Location: ../../frontend/credentials/signup.php");
             }
             else
             {
-                $_SESSION['status'] = StatusCodes::ErrServer;
-                $_SESSION['dependencies'] = "FRONTEND";
-                header("Location: ../../frontend/credentials/signup.php");
+                $_SESSION['status'] = signup($conn, $username, $password, $account_type, $email);
+                if($_SESSION['status'] == StatusCodes::Success)
+                {
+                    $_SESSION['dependencies'] = "FRONTEND";
+                    header("Location: ../../frontend/credentials/login.php");
+                }
+                else
+                {
+                    $_SESSION['status'] = StatusCodes::ErrServer;
+                    $_SESSION['dependencies'] = "FRONTEND";
+                    header("Location: ../../frontend/credentials/signup.php");
+                }
             }
         }
     }
