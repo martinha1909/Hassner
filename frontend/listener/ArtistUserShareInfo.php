f27c271ffe6088bda5d4aec259073e1824fad851
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2021-10-14 19:40:14 -0600

    removed 2 columns

--- frontend/listener/ArtistUserShareInfo.php
+++ frontend/listener/ArtistUserShareInfo.php
@@ -88,9 +88,7 @@ $available_share = calculateArtistAvailableShares($_SESSION['selected_artist']);
                                 <th scope="col">Owned Shares</th>
                                 <th scope="col">Shares selling</th>
                                 <th scope="col">Shares requesting</th>
-                                <th scope="col">Artist</th>
                                 <th scope="col">Current price per share (q̶)</th>
-                                <th scope="col">Selling profit per share (q̶)</th>
                                 <th scope="col">Issued Shares</th>
                                 <th scope="col">Available Shares</th>
                             </tr>
@@ -103,9 +101,7 @@ $available_share = calculateArtistAvailableShares($_SESSION['selected_artist']);
                                 <th scope="row"><?php echo $_SESSION['shares_owned']; ?></th>
                                 <td><?php echo getAmountSharesSelling($_SESSION['username'], $_SESSION['selected_artist']); ?></td>
                                 <td><?php echo getAmountSharesRequesting($_SESSION['username'], $_SESSION['selected_artist']); ?></td>
-                                <td><?php echo $_SESSION['selected_artist']; ?></td>
                                 <td><?php echo round($_SESSION['current_pps']['price_per_share'], 2); ?></td>
-                                <td><?php echo $_SESSION['profit']; ?> (<?php echo $_SESSION['profit_rate']; ?>%)</td>
                                 <td><?php echo totalShareDistributed($_SESSION['selected_artist']); ?></td>
                                 <td><?php echo $_SESSION['available_shares']; ?></td>
                             </tr>
