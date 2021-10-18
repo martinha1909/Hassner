d45add2d3f460a20ecd09b588b607b5e1bf41033
Kai Wessner <kepwessner@gmail.com>
2021-10-17 18:13:59 -0600

    stylesheet

--- frontend/artist/PersonalPage.php
+++ frontend/artist/PersonalPage.php
@@ -11,6 +11,8 @@ include '../../backend/constants/LoggingModes.php';
 
   <title><?php echo $_SESSION['username']; ?> Page</title>
 
+  <link href="https://rsms.me/inter/inter-ui.css" rel="stylesheet">
+
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
   <link rel="stylesheet" href="../css/app.css" type="text/css">
   <link rel="stylesheet" href="../css/default.css" id="theme-color">
--- frontend/listener/PersonalPage.php
+++ frontend/listener/PersonalPage.php
@@ -10,6 +10,8 @@ include '../../backend/shared/PersonalPageHelpers.php';
   <!-- Title of the page -->
   <title><?php echo $_SESSION['username']; ?> Page</title>
 
+  <link href="https://rsms.me/inter/inter-ui.css" rel="stylesheet">
+
   <!-- Link to grab icons  -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
   <link rel="stylesheet" href="../css/app.css" type="text/css">
