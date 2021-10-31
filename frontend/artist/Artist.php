0df94ae2fd0902584d457d74b98bf3770455ca05
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2021-10-31 16:32:06 -0600

    move ArtistHelpers to include dir

--- frontend/artist/Artist.php
+++ frontend/artist/Artist.php
@@ -1,6 +1,6 @@
 <?php
     include '../../backend/control/Dependencies.php';
-    include '../../backend/artist/ArtistHelpers.php';
+    include '../../backend/artist/include/ArtistHelpers.php';
     include '../../backend/shared/include/MarketplaceHelpers.php';
     include '../../backend/shared/include/CampaignHelpers.php';
     include '../../backend/constants/ShareInteraction.php';
