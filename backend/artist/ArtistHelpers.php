328a3242a8af58b8cc34261186b0e89721a8173d
Kai Wessner <kepwessner@gmail.com>
2021-11-01 12:59:50 -0600

    replaced ticker with class

--- backend/artist/ArtistHelpers.php
+++ backend/artist/ArtistHelpers.php
@@ -219,38 +219,41 @@
     {
         $tickers = getAllArtistTickers();
         echo '
-                <div class="card">
-                    <div class="card-body text-dark">
-                        <marquee direction="left">
+                <div>
+                    <div class="marquee">
+                            <form action="../../backend/listener/TagToArtistShareInfoSwitcher.php" method = "post">
+                            <p>
         ';
         for($i = 0; $i < sizeof($tickers); $i++)
         {
             echo '
-                        <strong>'.$tickers[$i]->getTag().'</strong> '.$tickers[$i]->getPPS().'
+                                <input name = "artist_ticker" type = "submit" class="bold-ticker" aria-pressed="true" value ="'.$tickers[$i]->getTag().'"> '.$tickers[$i]->getPPS().'
             ';
             
             if($tickers[$i]->getChange() < 0)
             {
                 echo '
-                        <mark class="markup-red">-'.$tickers[$i]->getChange().'%</mark>
+                                <mark class="markup-red">-'.$tickers[$i]->getChange().'%</mark>
                 ';
             }
             else if($tickers[$i]->getChange() > 0)
             {
                 echo '
-                        <mark class="markup-green">+'.$tickers[$i]->getChange().'%</mark>
+                                <mark class="markup-green">+'.$tickers[$i]->getChange().'%</mark>
                 ';
             }
             if($tickers[$i]->getChange() == 0)
             {
                 echo '
-                        <mark>'.$tickers[$i]->getChange().'%</mark>
+                                <mark>'.$tickers[$i]->getChange().'%</mark>
                 ';
             }
+
+            echo " | ";
         }
         echo '
-                        </marquee>
-                    </div>
+                        </p>
+                    </form>
                 </div>
         ';
     }
