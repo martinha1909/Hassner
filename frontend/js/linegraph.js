ab66c799effc8a5775d86883be38b31e6880f8c4
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2021-11-04 21:30:41 -0600

    remove commented lines

--- frontend/js/linegraph.js
+++ frontend/js/linegraph.js
@@ -1,73 +1,11 @@
-// $(document).ready(function() {
-//     var json_transfer = JSON.parse(document.querySelector('#artist_user_share_info_script').getAttribute('artist_json'));
-//     var artist_market_tag = document.querySelector('#artist_user_share_info_script').getAttribute('artist_tag');
-//     var graph_option = document.querySelector('#artist_user_share_info_script').getAttribute('graph_option');
-//     var y_axis = [];
-//     var x_axis = [];
-//     var len = json_transfer.length;
-
-//     if(graph_option === "1D")
-//     {
-//         for (var i = 0; i < len; i++) {
-//             y_axis.push(json_transfer[i].price_per_share);
-//             x_axis.push(json_transfer[i].time_recorded);
-//         }
-//     }
-//     else if(graph_option === "5D")
-//     {
-//         for (var i = 0; i < len; i++) {
-//             y_axis.push(json_transfer[i].price_per_share);
-//             x_axis.push(json_transfer[i].time_recorded);
-//         }
-//     }
-
-    // var ctx = $("#mycanvas");
-
-    // var data = {
-    //     labels : x_axis,
-    //     datasets : [
-    //         {
-    //             label : json_transfer[0].artist_username,
-    //             data : y_axis,
-                // backgroundColor : "#0a60d0",
-                // borderColor : "#0a60d0",
-                // fill : false,
-                // lineTension : 0,
-                // pointRadius : 5
-    //         }
-    //     ]
-    // };
-
-    // var options = {
-    //     title : {
-    //         display : true,
-    //         position : "top",
-    //         text : artist_market_tag + " (" + json_transfer[0].artist_username + ")",
-    //         fontSize : 18,
-    //         fontColor : "#e2cda9ff"
-    //     },
-    //     legend : {
-    //         display : true,
-    //         position : "bottom"
-    //     }
-    // };
-
-    // var chart = new Chart( ctx, {
-    //     type : "line",
-    //     data : data,
-    //     options : options
-    // } );
-
-// });
-
 $(document).ready(function(){
     $.ajax({
         url : "http://localhost:8080/Hassner/backend/graph/LineGraphData.php",
         type : "GET",
         success : function(data){
             // console.log(data);
-            var artist_market_tag = document.querySelector('#artist_user_share_info_script').getAttribute('artist_tag');
             var graph_option = document.querySelector('#artist_user_share_info_script').getAttribute('graph_option');
+            var artist_market_tag = document.querySelector('#artist_user_share_info_script').getAttribute('artist_tag');
             var y_axis = [];
             var x_axis = [];
             var len = data.length;
