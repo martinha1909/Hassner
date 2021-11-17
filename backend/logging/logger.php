9c31b08ccadc7db6ab13c7e83760a8bad2284566
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2021-11-16 19:58:49 -0700

    merge db logging with hx logging

--- backend/logging/logger.php
+++ backend/logging/logger.php
@@ -1,31 +1,4 @@
 <?php
-    function db_info($conn, $type, $msg, $date)
-    {
-        $sql = "INSERT INTO info_log (log_type, message, date_logged)
-                VALUES(?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param('sss', $type, $msg, $date);
-        $stmt->execute();
-    }
-
-    function db_error($conn, $type, $msg, $date)
-    {
-        $sql = "INSERT INTO error_log (log_type, message, date_logged)
-                VALUES(?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param('sss', $type, $msg, $date);
-        $stmt->execute();
-    }
-
-    function db_debug($conn, $type, $msg, $date)
-    {
-        $sql = "INSERT INTO debug_log (log_type, message, date_logged)
-                VALUES(?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param('sss', $type, $msg, $date);
-        $stmt->execute();
-    }
-
     function hx_error($type, $msg, $path)
     {
         $conn = connect();
@@ -34,8 +7,13 @@
         $log_msg = '['.$type.'] '.$msg."\r\n";
         $log_file = $path.'/error.log';
 
+        $sql = "INSERT INTO error_log (log_type, message, date_logged)
+                VALUES(?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('sss', $type, $msg, $date);
+        $stmt->execute();
+
         error_log('['.date("F j, Y, g:i a e O").']'.$log_msg, 3,  $log_file);
-        db_error($conn, $type, $msg, date('Y-m-d H:i:s'));
     }
 
     function hx_info($type, $msg, $path)
@@ -46,8 +24,13 @@
         $log_msg = '['.$type.'] '.$msg."\r\n";
         $log_file = $path.'/info.log';
 
+        $sql = "INSERT INTO info_log (log_type, message, date_logged)
+                VALUES(?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('sss', $type, $msg, $date);
+        $stmt->execute();
+
         error_log('['.date("F j, Y, g:i a e O").']'.$log_msg, 3,  $log_file);
-        db_info($conn, $type, $msg, date('Y-m-d H:i:s'));
     }
 
     function hx_debug($type, $msg, $path)
@@ -58,7 +41,12 @@
         $log_msg = '['.$type.'] '.$msg."\r\n";
         $log_file = $path.'/debug.log';
 
+        $sql = "INSERT INTO debug_log (log_type, message, date_logged)
+                VALUES(?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('sss', $type, $msg, $date);
+        $stmt->execute();
+
         error_log('['.date("F j, Y, g:i a e O").']'.$log_msg, 3,  $log_file);
-        db_debug($conn, $type, $msg, date('Y-m-d H:i:s'));
     }
 ?>
