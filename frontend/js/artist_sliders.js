bdff02c9d1e55fc6661cb33feac9aa684a9176bc
Finn <finnmorin@gmail.com>
2021-11-20 14:51:03 -0800

    Big slide

--- frontend/js/artist_sliders.js
+++ frontend/js/artist_sliders.js
@@ -1,46 +1,83 @@
 $( function() {  
     // Buy slider init
-    $( "#buy_slider" ).slider({
+    $( "#buy_limit" ).slider({
       range: true,
       min: 0,
       max: 500,
-      values: [ 75, 300 ], // TODO: ajax Query db, get min and max shares that can be bought/sold.
+      values: [ 0, 500 ], // TODO: ajax Query db, get min and max limit
       slide: function( event, ui ) {
-        $( "#buy_min" ).val(ui.values[0]);
-        $( "#buy_max" ).val(ui.values[1]);
+        min = ui.values[0];
+        max = ui.values[1];
+        if(min == 0 && max == 500){
+          $("#buy_tip").text("Without limits the next available share(s) will be purchased");
+        }
+        else if (min > 0 && max == 500){
+          $("#buy_tip").text("The buy order will be executed while the price is below " + min);
+        }
+        else if (min > 0 && max < 500){
+          $("#buy_tip").text("The buy order will be executed while the price is between " + min + " and " + max);
+        }
+        else if (min == 0 && max < 500){
+          $("#buy_tip").text("The buy order will be executed while the price is above " + max);
+        }
+        $("#buy_cost").val("$" + min*$("#buy_num").slider("value") + " - $" + max*$("#buy_num").slider("value"));
       }
     });
-    $( "#buy_min" ).val($("#buy_slider").slider("values", 0));
-    $( "#buy_max" ).val($("#buy_slider").slider("values", 1));
+    $( "#buy_num_shares" ).val($("#buy_limit").slider("values", 0));
+    $( "#buy_cost" ).val($("#buy_limit").slider("values", 1));
 
-    $("#buy_min").change(function() {
-      $("#buy_slider").slider("values",0,$(this).val());
-    });
-    $("#buy_max").change(function() {
-      $("#buy_slider").slider("values",1,$(this).val());
-    });
 
 
     // Sell slider init
-    $( "#sell_slider" ).slider({
+    $( "#sell_limit" ).slider({
       range: true,
       min: 0,
       max: 500,
-      values: [ 75, 300 ], // TODO: ajax Query db, get min and max shares that can be bought/sold.
+      values: [ 0, 500 ], // TODO: ajax Query db, get min and max limit
       slide: function( event, ui ) {
-        $( "#sell_min" ).val(ui.values[0]);
-        $( "#sell_max" ).val(ui.values[1]);
+        min = ui.values[0];
+        max = ui.values[1];
+        if(min == 0 && max == 500){
+          $("#sell_tip").text("Without limits the next available share(s) will be purchased");
+        }
+        else if (min > 0 && max == 500){
+          $("#sell_tip").text("The sell order will be executed while the price is below " + min);
+        }
+        else if (min > 0 && max < 500){
+          $("#sell_tip").text("The sell order will be executed while the price is between " + min + " and " + max);
+        }
+        else if (min == 0 && max < 500){
+          $("#sell_tip").text("The sell order will be executed while the price is above " + max);
+        }
+        $("#sell_cost").val("$" + min*$("#sell_num").slider("value") + " - $" + max*$("#sell_num").slider("value"));
       }
     });
-    $( "#sell_min" ).val($("#sell_slider").slider("values", 0));
-    $( "#sell_max" ).val($("#sell_slider").slider("values", 1));
+    $( "#sell_min" ).val($("#sell_limit").slider("values", 0));
+    $( "#sell_max" ).val($("#sell_limit").slider("values", 1));
 
-    $("#sell_min").change(function() {
-      $("#sell_slider").slider("values",0,$(this).val());
-    });
-    $("#sell_max").change(function() {
-      $("#sell_slider").slider("values",1,$(this).val());
-    });
+    // # Shares to buy slider
+    $("#buy_num").slider({
+      range: "min",
+      min: 0,
+      max: 500,
+      value: 0,
+      slide: function( event, ui ) {
+        $("#buy_num_shares").val(ui.value);
+        $("#buy_cost").val("$" + ui.value*$("#buy_limit").slider("values", 0) + " - $" + ui.value*$("#buy_limit").slider("values", 1));
+      }
+    })
+
+     // # Shares to sell slider
+     $("#sell_num").slider({
+      range: "min",
+      min: 0,
+      max: 500,
+      value: 0,
+      slide: function( event, ui ) {
+        $("#sell_num_shares").val(ui.value);
+        $("#sell_cost").val("$" + ui.value*$("#sell_limit").slider("values", 0) + " - $" + ui.value*$("#sell_limit").slider("values", 1));
+      }
+    })
 
     // Accoridon init
     $( "#buy_accordion" ).accordion({
