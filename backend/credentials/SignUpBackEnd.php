be8a8d32dc6ed409c2ac3b41dcd1a5745b5e1939
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2021-11-24 18:23:17 -0700

    use die

--- backend/credentials/SignUpBackEnd.php
+++ backend/credentials/SignUpBackEnd.php
@@ -41,7 +41,7 @@
         }
         else
         {
-            $ticker_error = FALSE;
+            // $ticker_error = FALSE;
             $usr_res = searchAccount($conn, $username);
             $email_res = searchEmail($conn, $email);
 
@@ -51,13 +51,14 @@
                 // Validate ticker
                 if($ticker_res->num_rows > 0)
                 {
-                    $ticker_error = TRUE;
+                    // $ticker_error = TRUE;
                     $msg = $ticker." is already taken";
                     hx_error(HX::SIGNUP, $msg);
 
                     $_SESSION['status'] = StatusCodes::ErrTickerDuplicate;
                     $_SESSION['dependencies'] = "FRONTEND";
                     header("Location: ../../frontend/credentials/signup.php");
+                    die;
                 }
                 else if(strlen($ticker) != 4 ||
                         !is_numeric($ticker[0]) || 
@@ -65,59 +66,56 @@
                         !ctype_alpha($ticker[2]) ||
                         !ctype_alpha($ticker[3]))
                 {
-                    $ticker_error = TRUE;
+                    // $ticker_error = TRUE;
                     $msg = $ticker." does not start with 2 digits and end with 2 letters";
                     hx_error(HX::SIGNUP, $msg);
 
                     $_SESSION['status'] = StatusCodes::ErrTickerFormat;
                     $_SESSION['dependencies'] = "FRONTEND";
                     header("Location: ../../frontend/credentials/signup.php");
+                    die;
                 }
             }
-
-            if(!$ticker_error)
+            if($usr_res->num_rows > 0)
             {
-                if($usr_res->num_rows > 0)
-                {
-                    $msg = $username." is already taken";
-                    hx_error(HX::SIGNUP, $msg);
+                $msg = $username." is already taken";
+                hx_error(HX::SIGNUP, $msg);
 
-                    $_SESSION['status'] = StatusCodes::ErrUsername;
-                    $_SESSION['dependencies'] = "FRONTEND";
-                    header("Location: ../../frontend/credentials/signup.php");
-                }
-                else if($email_res->num_rows > 0)
-                {
-                    $msg = $email." is already taken";
-                    hx_error(HX::SIGNUP, $msg);
+                $_SESSION['status'] = StatusCodes::ErrUsername;
+                $_SESSION['dependencies'] = "FRONTEND";
+                header("Location: ../../frontend/credentials/signup.php");
+            }
+            else if($email_res->num_rows > 0)
+            {
+                $msg = $email." is already taken";
+                hx_error(HX::SIGNUP, $msg);
 
-                    $_SESSION['status'] = StatusCodes::ErrEmailDuplicate;
+                $_SESSION['status'] = StatusCodes::ErrEmailDuplicate;
+                $_SESSION['dependencies'] = "FRONTEND";
+                header("Location: ../../frontend/credentials/signup.php");
+            }
+            else
+            {
+                $_SESSION['status'] = signup($connPDO, $username, $password, $account_type, $email, $ticker);
+                if($_SESSION['status'] == StatusCodes::Success)
+                {
+                    $msg = $username." successfully signed up";
+                    hx_info(HX::SIGNUP, $msg);
+
+                    $msg = "sign up data: username: ".$username.", password: ".$password.", email: ".$email.", account type: ".$account_type.", ticker: ".$ticker;
+                    hx_debug(HX::SIGNUP, $msg);
+
                     $_SESSION['dependencies'] = "FRONTEND";
-                    header("Location: ../../frontend/credentials/signup.php");
+                    header("Location: ../../frontend/credentials/login.php");
                 }
                 else
                 {
-                    $_SESSION['status'] = signup($connPDO, $username, $password, $account_type, $email, $ticker);
-                    if($_SESSION['status'] == StatusCodes::Success)
-                    {
-                        $msg = $username." successfully signed up";
-                        hx_info(HX::SIGNUP, $msg);
+                    $msg = "Server error";
+                    hx_error(HX::SIGNUP, $msg);
 
-                        $msg = "sign up data: username: ".$username.", password: ".$password.", email: ".$email.", account type: ".$account_type.", ticker: ".$ticker;
-                        hx_debug(HX::SIGNUP, $msg);
-
-                        $_SESSION['dependencies'] = "FRONTEND";
-                        header("Location: ../../frontend/credentials/login.php");
-                    }
-                    else
-                    {
-                        $msg = "Server error";
-                        hx_error(HX::SIGNUP, $msg);
-
-                        $_SESSION['status'] = StatusCodes::ErrServer;
-                        $_SESSION['dependencies'] = "FRONTEND";
-                        header("Location: ../../frontend/credentials/signup.php");
-                    }
+                    $_SESSION['status'] = StatusCodes::ErrServer;
+                    $_SESSION['dependencies'] = "FRONTEND";
+                    header("Location: ../../frontend/credentials/signup.php");
                 }
             }
         }
