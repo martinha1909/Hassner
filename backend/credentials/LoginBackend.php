dbbc8d84cc9e4081db6f3956958461f23f7dba75
Finn <finnmorin@gmail.com>
2021-12-01 17:49:41 -0800

    Initial Commit

--- backend/credentials/LoginBackend.php
+++ backend/credentials/LoginBackend.php
@@ -8,6 +8,7 @@
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+
     $result = login($conn,$username,$password);
     if ($result->num_rows > 0) 
     {
@@ -16,33 +17,12 @@
 
         $_SESSION['account_type'] = $row['account_type'];
         $_SESSION['username'] = $row['username'];
-        $_SESSION['password'] = $row['password'];
         $_SESSION['id'] = $row['id'];
         $_SESSION['dependencies'] = "FRONTEND";
-        if($row['account_type'] == AccountType::User)
-        {
-            $msg = "User ".$username." just logged in";
-            hx_info(HX::LOGIN, $msg);
-
-            header("Location: ../../frontend/listener/Listener.php");
-            die;
-        }
-        else if($row['account_type'] == AccountType::Artist)
-        {
-            $msg = "Artist ".$username." just logged in";
-            hx_info(HX::LOGIN, $msg);
-
-            header("Location: ../../frontend/artist/Artist.php");
-            die;
-        }
-        else if($row['account_type'] == AccountType::Admin)
-        {
-            $msg = "Admin ".$username." just logged in";
-            hx_info(HX::LOGIN, $msg);
-
-            header("Location: ../../frontend/admin/Admin.php");
-            die;
-        }
+        echo(json_encode(array(            
+            "status"=> 1,
+            "msg"=> "Success"
+        )));
     }
     else
     {
@@ -50,11 +30,10 @@
         hx_error(HX::LOGIN, $msg);
 
         $_SESSION['dependencies'] = "FRONTEND";
-        $_SESSION['status'] = StatusCodes::ErrGeneric;
-        header("Location: ../../frontend/credentials/login.php");
+        echo(json_encode(array(            
+            "status"=> "0",
+            "msg"=> "Invalid Login"
+        )));
     }
-
-      
-
 
 ?>
