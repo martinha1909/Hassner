56c3db97267769e06f2d7c34be73f98f3df06b6a
martinha1909 <58370282+martinha1909@users.noreply.github.com>
2021-12-23 12:57:31 -0700

    improve (#120)

--- backend/listener/SearchArtistSwitcher.php
+++ backend/listener/SearchArtistSwitcher.php
@@ -6,6 +6,19 @@
     $_SESSION['artist_found'] = FALSE;
     $artist_name = $_POST['artist_search'];
 
+    if(strlen($artist_name) == 4)
+    {
+        $result = searchArtistByTicker($conn, $artist_name);
+        if($result->num_rows > 0)
+        {
+            $row = $result->fetch_assoc();
+            $_SESSION['selected_artist'] = $row['artist_username'];
+            $_SESSION['artist_found'] = TRUE;
+            header("Location: ../../frontend/listener/ArtistUserShareInfo.php");
+            die;
+        }
+    }
+
     $result = searchArtist($conn, $artist_name);
     if($result->num_rows > 0)
     {
