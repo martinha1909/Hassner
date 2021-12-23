3624b51cc34b3a28eb879b87fbcafda046cb0728
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2021-12-23 14:19:04 -0700

    fix session error

--- frontend/shared/LandingPage.php
+++ frontend/shared/LandingPage.php
@@ -4,27 +4,27 @@
     include '../../backend/control/Dependencies.php';
 
     if($_SESSION['account_type'] == AccountType::User)
-        {
-            $msg = "User ".$_SESSION['username']." just logged in";
-            hx_info(HX::LOGIN, $msg);
+    {
+        $msg = "User ".$_SESSION['username']." just logged in";
+        hx_info(HX::LOGIN, $msg);
 
-            header("Location: ../../frontend/listener/Listener.php");
-            die;
-        }
-        else if($row['account_type'] == AccountType::Artist)
-        {
-            $msg = "Artist ".$_SESSION['username']." just logged in";
-            hx_info(HX::LOGIN, $msg);
+        header("Location: ../../frontend/listener/Listener.php");
+        die;
+    }
+    else if($_SESSION['account_type'] == AccountType::Artist)
+    {
+        $msg = "Artist ".$_SESSION['username']." just logged in";
+        hx_info(HX::LOGIN, $msg);
 
-            header("Location: ../../frontend/artist/Artist.php");
-            die;
-        }
-        else if($row['account_type'] == AccountType::Admin)
-        {
-            $msg = "Admin ".$_SESSION['username']." just logged in";
-            hx_info(HX::LOGIN, $msg);
+        header("Location: ../../frontend/artist/Artist.php");
+        die;
+    }
+    else if($_SESSION['account_type'] == AccountType::Admin)
+    {
+        $msg = "Admin ".$_SESSION['username']." just logged in";
+        hx_info(HX::LOGIN, $msg);
 
-            header("Location: ../../frontend/admin/Admin.php");
-            die;
-        }
+        header("Location: ../../frontend/admin/Admin.php");
+        die;
+    }
 ?>
