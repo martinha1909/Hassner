97e732a091611e22ea640c3be18736d76b434664
martinha1909 <58370282+martinha1909@users.noreply.github.com>
2022-01-14 14:38:35 -0700

    Listener trade history ajax handler (#138)

--- frontend/listener/ArtistUserShareInfo.php
+++ frontend/listener/ArtistUserShareInfo.php
@@ -383,6 +383,7 @@
     <script type="text/javascript" src="../js/graph/Chart.min.js"></script>
     <script type="text/javascript" src="../js/graph/linegraph.js"></script>
     <script type="text/javascript" src="../js/listener/artist_sliders.js"></script>
+    <script src="../js/listener/TradeHistory.js"></script>
     <script>
         var slider = document.getElementById("myRange");
         var output = document.getElementById("demo");
