cda7c531dec5ef1f7825dc1ec379fde1eb664446
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2022-01-16 19:08:07 -0700

    init

--- backend/credentials/SignUpBackEnd.php
+++ backend/credentials/SignUpBackEnd.php
@@ -2,6 +2,7 @@
     $_SESSION['dependencies'] = "BACKEND";
     include '../control/Dependencies.php';  
     include '../constants/StatusCodes.php';
+    include '../constants/AccountTypes.php';
 
     $conn = connect();
     $connPDO = connectPDO();
@@ -9,122 +10,131 @@
     $password = $_POST['password'];
     $email = trim($_POST['email']);
     $account_type = $_POST['account_type'];
-    if($account_type == AccountType::Artist)
-    {
-        $ticker = strtoupper(trim($_POST['ticker']));
-    }
-    else
-    {
-        $ticker = NULL;
-    }
 
-    // Email Verification
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
-    {
-        $msg = $email." is not a supported email";
-        hx_error(HX::SIGNUP, $msg);
+    $ticker = strtoupper(trim($_POST['ticker']));
+    echo(json_encode(array(            
+        "username"=> $username,
+        "password"=> $password,
+        "email"=> $email,
+        "account_type"=> $account_type,
+        "ticker"=> $ticker
+    )));
+    // if($account_type == AccountType::Artist)
+    // {
+    //     $ticker = strtoupper(trim($_POST['ticker']));
+    // }
+    // else
+    // {
+    //     $ticker = NULL;
+    // }
 
-        $_SESSION['status'] = StatusCodes::ErrEmailFormat;
-        $_SESSION['dependencies'] = "FRONTEND";
-        header("Location: ../../frontend/credentials/signup.php");
-    }
-    else if(!empty($username) && !empty($password) && !empty($email))
-    {
-        if(!ctype_alnum($username))
-        {
-            $msg = $username." contains a non-alphanumeric character";
-            hx_error(HX::SIGNUP, $msg);
+    // // Email Verification
+    // if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+    // {
+    //     $msg = $email." is not a supported email";
+    //     hx_error(HX::SIGNUP, $msg);
 
-            $_SESSION['status'] = StatusCodes::ErrUsernameFormat;
-            $_SESSION['dependencies'] = "FRONTEND";
-            header("Location: ../../frontend/credentials/signup.php");
-        }
-        else
-        {
-            $usr_res = searchAccount($conn, $username);
-            $email_res = searchEmail($conn, $email);
+    //     $_SESSION['status'] = StatusCodes::ErrEmailFormat;
+    //     $_SESSION['dependencies'] = "FRONTEND";
+    //     header("Location: ../../frontend/credentials/signup.php");
+    // }
+    // else if(!empty($username) && !empty($password) && !empty($email))
+    // {
+    //     if(!ctype_alnum($username))
+    //     {
+    //         $msg = $username." contains a non-alphanumeric character";
+    //         hx_error(HX::SIGNUP, $msg);
 
-            if($account_type == AccountType::Artist)
-            {
-                $ticker_res = searchTicker($conn, $ticker);
-                // Validate ticker
-                if($ticker_res->num_rows > 0)
-                {
-                    $msg = $ticker." is already taken";
-                    hx_error(HX::SIGNUP, $msg);
+    //         $_SESSION['status'] = StatusCodes::ErrUsernameFormat;
+    //         $_SESSION['dependencies'] = "FRONTEND";
+    //         header("Location: ../../frontend/credentials/signup.php");
+    //     }
+    //     else
+    //     {
+    //         $usr_res = searchAccount($conn, $username);
+    //         $email_res = searchEmail($conn, $email);
 
-                    $_SESSION['status'] = StatusCodes::ErrTickerDuplicate;
-                    $_SESSION['dependencies'] = "FRONTEND";
-                    header("Location: ../../frontend/credentials/signup.php");
-                    die;
-                }
-                else if(strlen($ticker) != 4 ||
-                        !is_numeric($ticker[0]) || 
-                        !is_numeric($ticker[1]) ||
-                        !ctype_alpha($ticker[2]) ||
-                        !ctype_alpha($ticker[3]))
-                {
-                    $msg = $ticker." does not start with 2 digits and end with 2 letters";
-                    hx_error(HX::SIGNUP, $msg);
+    //         if($account_type == AccountType::Artist)
+    //         {
+    //             $ticker_res = searchTicker($conn, $ticker);
+    //             // Validate ticker
+    //             if($ticker_res->num_rows > 0)
+    //             {
+    //                 $msg = $ticker." is already taken";
+    //                 hx_error(HX::SIGNUP, $msg);
 
-                    $_SESSION['status'] = StatusCodes::ErrTickerFormat;
-                    $_SESSION['dependencies'] = "FRONTEND";
-                    header("Location: ../../frontend/credentials/signup.php");
-                    die;
-                }
-            }
+    //                 $_SESSION['status'] = StatusCodes::ErrTickerDuplicate;
+    //                 $_SESSION['dependencies'] = "FRONTEND";
+    //                 header("Location: ../../frontend/credentials/signup.php");
+    //                 die;
+    //             }
+    //             else if(strlen($ticker) != 4 ||
+    //                     !is_numeric($ticker[0]) || 
+    //                     !is_numeric($ticker[1]) ||
+    //                     !ctype_alpha($ticker[2]) ||
+    //                     !ctype_alpha($ticker[3]))
+    //             {
+    //                 $msg = $ticker." does not start with 2 digits and end with 2 letters";
+    //                 hx_error(HX::SIGNUP, $msg);
 
-            if($usr_res->num_rows > 0)
-            {
-                $msg = $username." is already taken";
-                hx_error(HX::SIGNUP, $msg);
+    //                 $_SESSION['status'] = StatusCodes::ErrTickerFormat;
+    //                 $_SESSION['dependencies'] = "FRONTEND";
+    //                 header("Location: ../../frontend/credentials/signup.php");
+    //                 die;
+    //             }
+    //         }
 
-                $_SESSION['status'] = StatusCodes::ErrUsername;
-                $_SESSION['dependencies'] = "FRONTEND";
-                header("Location: ../../frontend/credentials/signup.php");
-            }
-            else if($email_res->num_rows > 0)
-            {
-                $msg = $email." is already taken";
-                hx_error(HX::SIGNUP, $msg);
+    //         if($usr_res->num_rows > 0)
+    //         {
+    //             $msg = $username." is already taken";
+    //             hx_error(HX::SIGNUP, $msg);
 
-                $_SESSION['status'] = StatusCodes::ErrEmailDuplicate;
-                $_SESSION['dependencies'] = "FRONTEND";
-                header("Location: ../../frontend/credentials/signup.php");
-            }
-            else
-            {
-                $_SESSION['status'] = signup($connPDO, $username, $password, $account_type, $email, $ticker);
-                if($_SESSION['status'] == StatusCodes::Success)
-                {
-                    $msg = $username." successfully signed up";
-                    hx_info(HX::SIGNUP, $msg);
+    //             $_SESSION['status'] = StatusCodes::ErrUsername;
+    //             $_SESSION['dependencies'] = "FRONTEND";
+    //             header("Location: ../../frontend/credentials/signup.php");
+    //         }
+    //         else if($email_res->num_rows > 0)
+    //         {
+    //             $msg = $email." is already taken";
+    //             hx_error(HX::SIGNUP, $msg);
 
-                    $msg = "sign up data: username: ".$username.", password: ".$password.", email: ".$email.", account type: ".$account_type.", ticker: ".$ticker;
-                    hx_debug(HX::SIGNUP, $msg);
+    //             $_SESSION['status'] = StatusCodes::ErrEmailDuplicate;
+    //             $_SESSION['dependencies'] = "FRONTEND";
+    //             header("Location: ../../frontend/credentials/signup.php");
+    //         }
+    //         else
+    //         {
+    //             $_SESSION['status'] = signup($connPDO, $username, $password, $account_type, $email, $ticker);
+    //             if($_SESSION['status'] == StatusCodes::Success)
+    //             {
+    //                 $msg = $username." successfully signed up";
+    //                 hx_info(HX::SIGNUP, $msg);
 
-                    $_SESSION['dependencies'] = "FRONTEND";
-                    header("Location: ../../frontend/credentials/login.php");
-                }
-                else
-                {
-                    $msg = "Server error";
-                    hx_error(HX::SIGNUP, $msg);
+    //                 $msg = "sign up data: username: ".$username.", password: ".$password.", email: ".$email.", account type: ".$account_type.", ticker: ".$ticker;
+    //                 hx_debug(HX::SIGNUP, $msg);
 
-                    $_SESSION['status'] = StatusCodes::ErrServer;
-                    $_SESSION['dependencies'] = "FRONTEND";
-                    header("Location: ../../frontend/credentials/signup.php");
-                }
-            }
-        }
-    }
-    else
-    {
-        $msg = "Empty input";
-        hx_error(HX::SIGNUP, $msg);
+    //                 $_SESSION['dependencies'] = "FRONTEND";
+    //                 header("Location: ../../frontend/credentials/login.php");
+    //             }
+    //             else
+    //             {
+    //                 $msg = "Server error";
+    //                 hx_error(HX::SIGNUP, $msg);
 
-        $_SESSION['status'] = StatusCodes::ErrEmpty;
-        $_SESSION['dependencies'] = "FRONTEND";
-        header("Location: ../../frontend/credentials/signup.php");
-    }
+    //                 $_SESSION['status'] = StatusCodes::ErrServer;
+    //                 $_SESSION['dependencies'] = "FRONTEND";
+    //                 header("Location: ../../frontend/credentials/signup.php");
+    //             }
+    //         }
+    //     }
+    // }
+    // else
+    // {
+    //     $msg = "Empty input";
+    //     hx_error(HX::SIGNUP, $msg);
+
+    //     $_SESSION['status'] = StatusCodes::ErrEmpty;
+    //     $_SESSION['dependencies'] = "FRONTEND";
+    //     header("Location: ../../frontend/credentials/signup.php");
+    // }
 ?>
--- frontend/js/credentials/signup.js
+++ frontend/js/credentials/signup.js
@@ -1,3 +1,39 @@
+function signup()
+{
+    $.ajax({
+        type: "POST",
+        url: window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/Hassner/backend/credentials/SignUpBackEnd.php",
+        data: {
+            username: $("#signup_username").val(),
+            password: $("#signup_pwd").val(),
+            email: $("#signup_email").val(),
+            account_type: $("#signup_account_type").val(),
+            ticker: $("#signupTicker").val()
+        },
+        async: false,
+        dataType: "json",
+        success: function(data){
+            console.log(data);
+        },
+        error: function(data){
+
+        }
+    });
+}
+
+$( function() {
+    $("#signup_btn").click(signup);
+});
+
+$(document).keypress(function (e) {
+    var key = e.which;
+    if(key == 13)  // the enter key code
+     {
+       $('#signup_btn').click();
+       return false;  
+     }
+});
+
 $(document).ready(function() {
     $(".artistRadio").hide();
     $('input[type="radio"]').click(function(){
