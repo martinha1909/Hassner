f713dfb60d181168b1c397f62361b3cbda614be4
Kai Wessner <kepwessner@gmail.com>
2022-01-16 19:26:47 -0700

    logo and logout

--- frontend/listener/Listener.php
+++ frontend/listener/Listener.php
@@ -35,6 +35,7 @@
 
     <!-- Bootstrap CSS / Color Scheme -->
     <link rel="icon" href="../../frontend/Images/hx_tmp_2.ico" type="image/ico">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/default.css" id="theme-color">
     <link rel="stylesheet" href="../css/menu.css" id="theme-color">
@@ -373,7 +374,7 @@
     </section>
 
     <a class="btn btn-success py-2" type="submit" role="button" aria-pressed="true" name="button" href="../credentials/login.php">Log out</a>
-
+ 
     <!--scroll to top-->
     <div class="scroll-top">
         <i class="fa fa-angle-up" aria-hidden="true"></i>
