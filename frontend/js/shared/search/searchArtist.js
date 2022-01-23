fbf9879aa909c5d054d3dd7b42733dd55a1e9c26
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2022-01-22 21:58:25 -0700

    remove key 10

--- frontend/js/shared/search/searchArtist.js
+++ frontend/js/shared/search/searchArtist.js
@@ -1,7 +1,7 @@
 $( function() {
     $('#search_artist').each(function() {
         $(this).find('#submit_search_form').keypress(function(e) {
-            if(e.which == 10 || e.which == 13) {
+            if(e.which == 13) {
                 this.form.submit();
             }
         });
