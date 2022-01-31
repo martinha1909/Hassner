86efe772a768bcf1e1e1362d8a1fd871cf1e7c3e
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2022-01-31 11:50:02 -0700

    finish sell sliders restrictions

--- backend/control/Queries.php
+++ backend/control/Queries.php
@@ -544,12 +544,34 @@
         {
             $result = 0;
 
-            $sql = "SELECT quantity
+            $sql = "SELECT id, quantity
                     FROM buy_order 
                     WHERE artist_username = ? AND user_username != ? AND siliqas_requested = -1 AND buy_stop <= ? AND buy_stop != -1
                     ORDER BY date_posted ASC";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param('ssd', $artist_username, $user_username, $stop);
+            if($stmt->execute() == true)
+            {
+                $result = $stmt->get_result();
+            }
+            else
+            {
+                hx_error(HX::DB, "db error occured: ".$conn->mysqli_error($conn));
+            }
+
+            return $result;
+        }
+
+        function searchQuantityLimitBuyOrders($conn, $user_username, $artist_username, $limit)
+        {
+            $result = 0;
+
+            $sql = "SELECT id, quantity
+                    FROM buy_order 
+                    WHERE artist_username = ? AND user_username != ? AND siliqas_requested = -1 AND buy_limit >= ?
+                    ORDER BY date_posted ASC";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('ssd', $artist_username, $user_username, $limit);
             if($stmt->execute() == true)
             {
                 $result = $stmt->get_result();
@@ -584,6 +606,28 @@
             return $result;
         }
 
+        function searchLimitOrderSharesSelling($conn, $user_username, $artist_username, $limit)
+        {
+            $result = 0;
+
+            $sql = "SELECT no_of_share
+                    FROM sell_order 
+                    WHERE artist_username = ? AND user_username = ? AND selling_price = -1 AND sell_limit = ?
+                    ORDER BY date_posted ASC";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('ssd', $artist_username, $user_username, $limit);
+            if($stmt->execute() == true)
+            {
+                $result = $stmt->get_result();
+            }
+            else
+            {
+                hx_error(HX::DB, "db error occured: ".$conn->mysqli_error($conn));
+            }
+
+            return $result;
+        }
+
         function searchNumOfSharesNoLimitStopSellOrders($conn, $user_username, $artist_username, $market_price)
         {
             $result = 0;
@@ -591,6 +635,28 @@
             $sql = "SELECT no_of_share
                     FROM sell_order 
                     WHERE artist_username = ? AND user_username != ? AND selling_price = ? AND sell_limit = -1 AND sell_stop = -1
+                    ORDER BY date_posted ASC";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('ssd', $artist_username, $user_username, $market_price);
+            if($stmt->execute() == true)
+            {
+                $result = $stmt->get_result();
+            }
+            else
+            {
+                hx_error(HX::DB, "db error occured: ".$conn->mysqli_error($conn));
+            }
+
+            return $result;
+        }
+
+        function searchQuantityNoLimitStopBuyOrders($conn, $user_username, $artist_username, $market_price)
+        {
+            $result = 0;
+
+            $sql = "SELECT id, quantity
+                    FROM buy_order 
+                    WHERE artist_username = ? AND user_username != ? AND siliqas_requested = ? AND buy_limit = -1 AND buy_stop = -1
                     ORDER BY date_posted ASC";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param('ssd', $artist_username, $user_username, $market_price);
@@ -758,6 +824,19 @@
         function searchSellOrderByArtistAndUser($conn, $user_username, $artist_username)
         {
             $sql = "SELECT * FROM sell_order WHERE artist_username = ? AND user_username = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('ss', $artist_username, $user_username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            return $result;
+        }
+
+        function searchMaxIDSellOrdersNotFromUser($conn, $user_username, $artist_username)
+        {
+            $result = 0;
+
+            $sql = "SELECT MAX(id) AS max_sell_order_id FROM sell_order WHERE artist_username = ? AND user_username != ?";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param('ss', $artist_username, $user_username);
             $stmt->execute();
--- backend/sliders/SellableShares.php
+++ backend/sliders/SellableShares.php
@@ -2,6 +2,7 @@
     header('Content-Type: application/json');
     $_SESSION['dependencies'] = "BACKEND";
     include '../control/Dependencies.php';
+    include '../shared/include/MarketplaceHelpers.php';
 
     $min_lim = $_POST['min_lim'];
     $max_lim = $_POST['max_lim'];
@@ -11,6 +12,7 @@
     $sellable_shares = 0;
     $total_shares_owned = 0;
     $total_shares_selling = 0;
+    $current_pps = getArtistPricePerShare($_SESSION['selected_artist']);
 
     if($chosen_min == $min_lim && $chosen_max == $max_lim)
     {
@@ -37,7 +39,6 @@
     }
     else if ($chosen_min > $min_lim && $chosen_max == $max_lim)
     {
-        $shares_selling_stop = 0;
         $matching_shares_requested = 0;
 
         //These are the shares from buyers that have their stop match with this sell order's stop
@@ -47,16 +48,6 @@
             while($row = $res_buy_stop->fetch_assoc())
             {
                 $matching_shares_requested += $row['quantity'];
-            }
-        }
-
-        //These are the shares that this user currently is selling at this chosen stop
-        $res_shares_selling_stop = searchStopOrderSharesSelling($conn, $_SESSION['username'], $_SESSION['selected_artist'], $chosen_min);
-        if($res_shares_selling_stop->num_rows > 0)
-        {
-            while($row = $res_shares_selling_stop->fetch_assoc())
-            {
-                $shares_selling_stop += $row['no_of_share'];
             }
         }
 
@@ -82,7 +73,7 @@
         }
 
         //Total number of shares available to sell
-        $shares_selling_stop_avai = $total_shares_owned - $total_shares_selling - $shares_selling_stop;
+        $shares_selling_stop_avai = $total_shares_owned - $total_shares_selling;
 
         //We do not let the user create a sell order at this chosen stop unless there is at least 1 matching buy order at this stop
         if($matching_shares_requested > 0)
@@ -99,11 +90,141 @@
     }
     else if ($chosen_min == $min_lim && $chosen_max < $max_lim)
     {
+        $matching_shares_requested = 0;
 
+        //These are the shares from buyers that have their limit match with this sell order's limit
+        $res_buy_limit = searchQuantityLimitBuyOrders($conn, $_SESSION['username'], $_SESSION['selected_artist'], $chosen_max);
+        if($res_buy_limit->num_rows > 0)
+        {
+            while($row = $res_buy_limit->fetch_assoc())
+            {
+                $matching_shares_requested += $row['quantity'];
+            }
+        }
+
+        //Total amount of shares the user has towards this current artist
+        $res_shares_owned = searchSharesInArtistShareHolders($conn, $_SESSION['username'], $_SESSION['selected_artist']);
+        if($res_shares_owned->num_rows > 0)
+        {
+            $row = $res_shares_owned->fetch_assoc();
+            $total_shares_owned = $row['shares_owned'];
+        }
+
+        //If the user owns some shares, check to see how many of them he/she is selling, limit and stop doesn't matter
+        if($total_shares_owned > 0)
+        {
+            $res_total_shares_selling = searchSharesSelling($conn, $_SESSION['username'], $_SESSION['selected_artist']);
+            if($res_total_shares_selling -> num_rows > 0)
+            {
+                while($row = $res_total_shares_selling->fetch_assoc())
+                {
+                    $total_shares_selling += $row['no_of_share'];
+                }
+            }
+        }
+
+        //Total number of shares available to sell
+        $shares_selling_limit_avai = $total_shares_owned - $total_shares_selling;
+
+        //We do not let the user create a sell order at this chosen stop unless there is at least 1 matching buy order at this stop
+        if($matching_shares_requested > 0)
+        {
+            if($shares_selling_limit_avai > $matching_shares_requested)
+            {
+                $sellable_shares = $matching_shares_requested;
+            }
+            else
+            {
+                $sellable_shares = $shares_selling_limit_avai;
+            }
+        }
     }
     else if ($chosen_min > $min_lim && $chosen_max < $max_lim)
     {
-        
+        $matching_shares_requested = 0;
+
+        $res_array_size = searchMaxIDSellOrdersNotFromUser($conn, $_SESSION['username'], $_SESSION['selected_artist']);
+        $max_arr_size = $res_array_size->fetch_assoc();
+        //Using a hashmap for quicker lookup
+        $already_matched_orders = array_fill(0, $max_arr_size['max_sell_order_id'], false);
+
+        //These are the shares from buyers that have their limit match with this sell order's limit
+        $res_buy_limit = searchQuantityLimitBuyOrders($conn, $_SESSION['username'], $_SESSION['selected_artist'], $chosen_max);
+        if($res_buy_limit->num_rows > 0)
+        {
+            while($row = $res_buy_limit->fetch_assoc())
+            {
+                if(!$already_matched_orders[$row['id']])
+                {
+                    $matching_shares_requested += $row['quantity'];
+                }
+                $already_matched_orders[$row['id']] = true;
+            }
+        }
+
+        //These are the shares from buyers that have their stop match with this sell order's stop
+        $res_buy_stop = searchQuantityStopBuyOrders($conn, $_SESSION['username'], $_SESSION['selected_artist'], $chosen_min);
+        if($res_buy_stop->num_rows > 0)
+        {
+            while($row = $res_buy_stop->fetch_assoc())
+            {
+                if(!$already_matched_orders[$row['id']])
+                {
+                    $matching_shares_requested += $row['quantity'];
+                }
+                $already_matched_orders[$row['id']] = true;
+            }
+        }
+
+        if($chosen_min > $current_pps || $chosen_min < $current_pps)
+        {
+            $res = searchQuantityNoLimitStopBuyOrders($conn, $_SESSION['username'], $_SESSION['selected_artist'], $current_pps);
+            while($row = $res->fetch_assoc())
+            {
+                if(!$already_matched_orders[$row['id']])
+                {
+                    $matching_shares_requested += $row['quantity'];
+                }
+                $already_matched_orders[$row['id']] = true;
+            }
+        }
+
+        //Total amount of shares the user has towards this current artist
+        $res_shares_owned = searchSharesInArtistShareHolders($conn, $_SESSION['username'], $_SESSION['selected_artist']);
+        if($res_shares_owned->num_rows > 0)
+        {
+            $row = $res_shares_owned->fetch_assoc();
+            $total_shares_owned = $row['shares_owned'];
+        }
+
+        //If the user owns some shares, check to see how many of them he/she is selling, limit and stop doesn't matter
+        if($total_shares_owned > 0)
+        {
+            $res_total_shares_selling = searchSharesSelling($conn, $_SESSION['username'], $_SESSION['selected_artist']);
+            if($res_total_shares_selling -> num_rows > 0)
+            {
+                while($row = $res_total_shares_selling->fetch_assoc())
+                {
+                    $total_shares_selling += $row['no_of_share'];
+                }
+            }
+        }
+
+        //Total number of shares available to sell
+        $shares_selling_avai = $total_shares_owned - $total_shares_selling;
+
+        //We do not let the user create a sell order at this chosen stop unless there is at least 1 matching buy order at this stop
+        if($matching_shares_requested > 0)
+        {
+            if($shares_selling_avai > $matching_shares_requested)
+            {
+                $sellable_shares = $matching_shares_requested;
+            }
+            else
+            {
+                $sellable_shares = $shares_selling_avai;
+            }
+        }
     }
 
     print json_encode($sellable_shares);
