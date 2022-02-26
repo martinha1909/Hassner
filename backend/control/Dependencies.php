5afbb3e56173db5f3d672c8ee13c6cd0cebc8f2d
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2022-02-25 18:26:43 -0700

    use ===

--- backend/control/Dependencies.php
+++ backend/control/Dependencies.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    if(!isset($_SESSION['username']) || $_SESSION['username'] == 0)
+    if($_SESSION['username'] === 0)
     {
         header("Location: ../credentials/index.php");
         die;
