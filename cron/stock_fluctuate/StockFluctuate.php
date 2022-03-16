f89295b4ff7883099761a6fc4e98ea220b51f955
Martin Ha <58370282+martinha1909@users.noreply.github.com>
2022-03-15 18:08:32 -0600

    messages to output to a file

--- cron/stock_fluctuate/StockFluctuate.php
+++ cron/stock_fluctuate/StockFluctuate.php
@@ -26,13 +26,17 @@
     $connPDO = connectPDO();
     $conn = connect();
     
+    echo "Date ran: ".$current_date."\n";
     $all_platform_artists = getAllArtist($conn);
     for($i = 0; $i < sizeof($all_platform_artists); $i++)
     {
+        echo "Artist: ".$all_platform_artists[$i]."\n";
         $hours_ago = date("Y-m-d H:i:s", strtotime("-59 mins"));
 
         $num_open_buy_orders = getArtistOpenBuyOrdersWithinInterval($conn, $all_platform_artists[$i], $hours_ago, $current_date);
+        echo "Found ".$num_open_buy_orders." shares requesting in the past hour\n";
         $num_open_sell_orders = getArtistOpenSellOrdersWithinInterval($conn, $all_platform_artists[$i], $hours_ago, $current_date);
+        echo "Found ".$num_open_sell_orders." shares selling in the past hour\n";
         $new_artist_pps = calculatePPSBySupplyDemand($conn, 
                                                      $all_platform_artists[$i], 
                                                      $num_open_buy_orders, 
@@ -41,9 +45,18 @@
         if($new_artist_pps > 0)
         {
             updateArtistPPS($connPDO, $all_platform_artists[$i], $new_artist_pps);
+            echo "Stock price updated to ".$new_artist_pps."\n";
             updateMarketPriceOrderToPPS($new_artist_pps, $all_platform_artists[$i]);
         }
+        else
+        {
+            echo "Received unexpected value of -1, did not update stock price\n";
+        }
+
+        echo "------------------------------------\n";
     }
+
+    echo "////////////////////////////////////////////////////////////////////////////\n";
 
     closeCon($conn);
 ?>
--- cron/stock_fluctuate/StockFluctuateNoOrders.php
+++ cron/stock_fluctuate/StockFluctuateNoOrders.php
@@ -29,9 +29,11 @@
     $connPDO = connectPDO();
     $conn = connect();
     
+    echo "Date ran: ".$current_date."\n";
     $all_platform_artists = getAllArtist($conn);
     for($i = 0; $i < sizeof($all_platform_artists); $i++)
     {
+        echo "Artist: ".$all_platform_artists[$i]."\n";
         $hours_ago = date("Y-m-d H:i:s", strtotime("-239 minutes"));
 
         $num_open_buy_orders = getArtistOpenBuyOrdersWithinInterval($conn, $all_platform_artists[$i], $hours_ago, $current_date);
@@ -39,17 +41,27 @@
         $num_of_activities = searchBuyHistoryWithinInterval($conn, $all_platform_artists[$i], $hours_ago, $current_date)->num_rows;
         if($num_open_buy_orders === 0 && $num_open_sell_orders === 0 && $num_of_activities === 0)
         {
+            echo "No activities found in the past 4 hours, decreasing stock price slightly...\n";
             //If there are activities or orders for the past 4 hours, slightly decrease the price due to higher supply
             $new_artist_pps = decreaseStockPriceSlightly($conn, $all_platform_artists[$i]);
 
             if($new_artist_pps > 0)
             {
                 updateArtistPPS($connPDO, $all_platform_artists[$i], $new_artist_pps);
+                echo "Stock price updated to ".$new_artist_pps."\n";
                 //still need to update any orders that were created more than 4 hours ago here
                 updateMarketPriceOrderToPPS($new_artist_pps, $all_platform_artists[$i]);
             }
         }
+        else
+        {
+            echo "Trades found in the last 4 hours, skip changing stock price...\n";
+        }
+        
+        echo "------------------------------------\n";
     }
+
+    echo "////////////////////////////////////////////////////////////////////////////\n";
 
     closeCon($conn);
 ?>
