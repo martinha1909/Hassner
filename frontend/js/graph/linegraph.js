6831d3f53b6dab8e875bba5ef38a9817921bb0b1
martinha1909 <58370282+martinha1909@users.noreply.github.com>
2022-03-24 14:11:52 -0600

    Update linegraph.js

--- frontend/js/graph/linegraph.js
+++ frontend/js/graph/linegraph.js
@@ -154,11 +154,6 @@ function JSONToAJAX(graph_option)
                                     {
                                         return value;
                                     }
-                                    //keep the x-axis clean by only display 2 time slots per day for 5D graph
-                                    if(value === "08:00" || value === "16:00")
-                                    {
-                                        return value;
-                                    }
                                 }
                                 else
                                 {
